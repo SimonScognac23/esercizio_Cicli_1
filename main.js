d731149dfd43627aa5a6b0ae667ee3a3976a870d
SimonScognac23 <scognac23@gmail.com>
2024-11-10 16:28:30 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+
+let num = 2;
+
+console.log(`Tabellina numero ${num}:`); // Stampa l'intestazione della tabellina con il numero scelto
+
+for (let i = 1; i <= 10; i++) {   // Inizia un ciclo for da 1 a 10 per generare la tabellina
+    console.log(num + " x " + i + " = " + (num * i));    // Stampa il risultato di num moltiplicato per i
+
+}
